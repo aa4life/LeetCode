5dec0a3f8fa42ad1bd97ac19f4401e6e015615cf
willy <rogerwilly@gmail.com>
2018-11-28 20:12:12 +0100

    78

---
+++ 78 Subsets/78 Subsets.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = [[]]    
+           
+        for n in nums:
+            result += [r + [n] for r in result]
+            
+        return result
